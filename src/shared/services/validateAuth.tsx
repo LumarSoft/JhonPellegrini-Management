95b99a5930c514739c194e387c89a4943bf54d7e
marcebenitez2 <marcebenitez0607@gmail.com>
2024-03-26 14:57:08 -0300

    feat: cambio en la logica de redireccionamiento para la autenticacion

--- src/shared/services/validateAuth.tsx
+++ src/shared/services/validateAuth.tsx
@@ -1,19 +1,31 @@
 "use client";
 import { useRouter } from "next/navigation";
 import { useUserStore } from "../context/userStore";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 export const ValidateAuth = ({ children }: { children: React.ReactNode }) => {
   const { user } = useUserStore();
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    if (!user || user === "") {
-      router.push("/");
-    } else {
-      router.push("/dashboard");
-    }
+    const handleUser = () => {
+      if (user === null) {
+        router.push("/");
+      } else if (user === "") {
+        router.push("/");
+      } else {
+        router.push("/dashboard");
+      }
+      setIsLoading(false);
+    };
+
+    handleUser();
   }, [user, router]);
+
+  if (isLoading) {
+    return <div>Cargando...</div>;
+  }
 
   return <>{children}</>;
 };
